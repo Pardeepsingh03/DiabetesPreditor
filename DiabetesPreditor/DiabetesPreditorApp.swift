cb882969e08b7b1644d4c24ba6adee938588feb0
Pardeepsingh03 <ps454075@gmail.com>
2025-07-15 12:55:09 +0100

    Initial Commit

---
+++ DiabetesPreditor/DiabetesPreditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetesPreditorApp.swift
+//  DiabetesPreditor
+//
+//  Created by Parry  on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiabetesPreditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiabetesPreditorTests/DiabetesPreditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetesPreditorTests.swift
+//  DiabetesPreditorTests
+//
+//  Created by Parry  on 15/07/2025.
+//
+
+import Testing
+@testable import DiabetesPreditor
+
+struct DiabetesPreditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
